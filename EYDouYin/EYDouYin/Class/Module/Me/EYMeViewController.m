f1d721cbe78aa42af8248691bb6a89632a8d679d
CoderEYLee <lieryangios@126.com>
2018-08-24 18:43:53 +0800

    增加 YYKit
     增加三方 YYKit

--- EYDouYin/EYDouYin/Class/Module/Me/EYMeViewController.m
+++ EYDouYin/EYDouYin/Class/Module/Me/EYMeViewController.m
@@ -7,15 +7,9 @@
 //
 
 #import "EYMeViewController.h"
-#import "ZFPlayer.h"
-#import "ZFAVPlayerManager.h"
-#import "ZFPlayerControlView.h"
+#import <YYKit.h>
 
 @interface EYMeViewController ()
-
-@property (nonatomic, strong) ZFPlayerController *player;
-@property (nonatomic, strong) UIView *containerView;
-@property (nonatomic, strong) ZFPlayerControlView *controlView;
 
 @end
 
@@ -26,39 +20,6 @@
     [super viewDidLoad];
     self.view.backgroundColor = EYRandomColor;
     EYLog(@"EYMeViewController--viewDidLoad");
-    
-    [self.view addSubview:self.containerView];
-    
-    ZFAVPlayerManager *playerManager = [[ZFAVPlayerManager alloc] init];
-    /// 播放器相关
-    self.player = [ZFPlayerController playerWithPlayerManager:playerManager containerView:self.containerView];
-    self.controlView.portraitControlView.fullScreenBtn.hidden = YES;
-    self.player.controlView = self.controlView;
-    self.player.assetURL = [NSURL URLWithString:@"https://www.apple.com/105/media/cn/mac/family/2018/46c4b917_abfd_45a3_9b51_4e3054191797/films/bruce/mac-bruce-tpl-cn-2018_1280x720h.mp4"];
  }
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    [self.player enterFullScreen:YES animated:YES];
-    
-}
-
-
-- (ZFPlayerControlView *)controlView {
-    if (!_controlView) {
-        _controlView = [[ZFPlayerControlView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-        _controlView.fastViewAnimated = YES;
-    }
-    return _controlView;
-}
-
-- (UIView *)containerView {
-    if (!_containerView) {
-        _containerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-        _containerView.backgroundColor = [UIColor orangeColor];
-    }
-    return _containerView;
-}
 
 @end
