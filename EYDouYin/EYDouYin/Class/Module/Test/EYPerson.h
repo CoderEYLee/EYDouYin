97adf90a1c238abd732b16c4e02d150372e460f6
CoderEYLee <lieryangios@126.com>
2018-08-31 10:38:11 +0800

    增加测试的类文件

---
+++ EYDouYin/EYDouYin/Class/Module/Test/EYPerson.h
@@ -0,0 +1,19 @@
+//
+//  EYPerson.h
+//  EYDouYin
+//
+//  Created by 李二洋 on 2018/8/30.
+//  Copyright © 2018年 李二洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EYPerson : NSObject
+
+@property (copy, nonatomic) NSString * name;
+
+@property (copy, nonatomic) NSString * user_id;
+
+@property (copy, nonatomic) NSString * age;
+
+@end
