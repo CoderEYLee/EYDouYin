9dfded8389d680fe123ac691a968c73d9a49b145
CoderEYLee <lieryangios@126.com>
2018-08-31 14:13:21 +0800

    增加程序的后台日志

--- EYDouYin/EYDouYin/Class/AppDelegate.m
+++ EYDouYin/EYDouYin/Class/AppDelegate.m
@@ -28,41 +28,33 @@
     // 3.显示窗口
     [self.window makeKeyAndVisible];
 
+    EYLog(@"1111111--->程序启动了");
+
     [NSThread sleepForTimeInterval:3];
 
     return YES;
 }
 
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // 启动/后台回来
+    EYLog(@"222222222--->启动/后台回来");
+}
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+- (void)applicationWillResignActive:(UIApplication *)application {
+    EYLog(@"333333333333--->程序准备进入后台");
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    EYLog(@"444444444444--->程序已经进入后台");
 }
 
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    EYLog(@"555555555555--->程序将会进入前台");
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)applicationWillTerminate:(UIApplication *)application {
+    EYLog(@"6666666666666--->程序被杀了");
 }
 
 @end
