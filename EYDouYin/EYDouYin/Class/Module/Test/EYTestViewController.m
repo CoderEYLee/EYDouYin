a70d197e27689402fc92cfef8534b4b22d9377ec
CoderEYLee <lieryangios@126.com>
2018-09-18 10:40:18 +0800

    修改测试界面的侧滑为全屏拖拽
    利用 runtime 设置为全屏幕侧滑

--- EYDouYin/EYDouYin/Class/Module/Test/EYTestViewController.m
+++ EYDouYin/EYDouYin/Class/Module/Test/EYTestViewController.m
@@ -16,40 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     self.view.backgroundColor = [UIColor whiteColor];
+    id target = self.navigationController.interactivePopGestureRecognizer.delegate;
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:NSSelectorFromString(@"handleNavigationTransition:")];
+    [self.view addGestureRecognizer:pan];
     
     UILabel * label = [[UILabel alloc] init];
     label.text = @"这是一个测试界面";
     [label sizeToFit];
     label.center = self.view.center;
     [self.view addSubview:label];
-
-    NSString * jsonName = @"ChinLab_en.json";
-    NSDictionary *dictionary = jsonName.ey_loadLocalFile;
-    NSString * value1 = dictionary[@"key1"];
-
-    NSLog(@"------dictionary:%@------", dictionary);
-
-    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, EYScreenWidth, 200)];
-    label1.backgroundColor = EYRandomColor;
-    label1.text = [NSString stringWithFormat:value1, @"AAAA", @"BBBB"];
-    label1.numberOfLines = 0;
-    [self.view addSubview:label1];
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    // 开启返回手势
-    if ([self.navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.navigationController.interactivePopGestureRecognizer.enabled = YES;
-    }
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [super touchesBegan:touches withEvent:event];
-
-    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
