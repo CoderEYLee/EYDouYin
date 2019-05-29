6382bdd1e990f98edd2b47155a46944e8e4a51b0
CoderEYLee <lieryangios@126.com>
2019-05-29 10:23:58 +0800

    修改关注界面

--- EYDouYin/EYDouYin/Class/Module/Like/EYLikeViewController.m
+++ EYDouYin/EYDouYin/Class/Module/Like/EYLikeViewController.m
@@ -17,8 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = EYColorRandom;
-    EYLog(@"EYLikeViewController--viewDidLoad");
+    //1. 初始化界面
+    [self setupUI];
+}
+
+//1. 初始化界面
+- (void)setupUI {
+    //1.导航
+    self.gk_navBackgroundColor = EYColorGray;
+    
+    //2.滚动视图
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, EYStatusBarAndNaviBarHeight, EYScreenWidth, EYScreenHeight - EYStatusBarAndNaviBarHeight - EYTabBarHomeIndicatorHeight)];
+    scrollView.backgroundColor = EYColorRandom;
+    scrollView.contentSize = CGSizeMake(EYScreenWidth * 2, EYScreenHeight);
+    scrollView.pagingEnabled = YES;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:scrollView];
+    
+    //3.底部 view
+    UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, EYScreenHeight - EYTabBarHomeIndicatorHeight, EYScreenWidth, EYTabBarHomeIndicatorHeight)];
+    bottomView.backgroundColor = EYColorBlue;
+    [self.view addSubview:bottomView];
 }
 
 @end
