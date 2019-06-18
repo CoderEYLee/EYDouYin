7212551a81e999883fc02e51100be35e55d80702
CoderEYLee <lieryangios@126.com>
2019-06-18 22:40:40 +0800

    保存

--- EYDouYin/EYDouYin/Class/Module/Me/EYMineViewController.m
+++ EYDouYin/EYDouYin/Class/Module/Me/EYMineViewController.m
@@ -8,7 +8,19 @@
 
 #import "EYMineViewController.h"
 
+@interface EYMineViewController() <UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>
+
+@property (nonatomic, weak) UIImageView *backImageView;
+
+@property (weak, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *arrayM;
+
+@end
+
 @implementation EYMineViewController
+//背景图的最大高度
+const CGFloat EYBackImageViewHeight = 250;
+static NSString *EYMineViewControllerCellID = @"EYMineViewControllerCellID";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -21,14 +33,108 @@
 - (void)setupUI {
     //1.隐藏分割线
     self.gk_navLineHidden = YES;
-    self.gk_navTitle = @"我的";
+//    self.gk_navTitle = @"我的";
     
     if (self.jumpType == EYJumpTypeDefault) {
-        //底部 view
+        //背景图片(放大)
+        UIImageView *backImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, EYScreenWidth, EYBackImageViewHeight)];
+        backImageView.image = [UIImage imageNamed:@"common_placeholder_mine"];
+        backImageView.contentMode = UIViewContentModeScaleAspectFill;
+        backImageView.layer.anchorPoint = CGPointMake(0.5, 0);
+        backImageView.center = CGPointMake(EYScreenWidth * 0.5, 0);
+        backImageView.size = CGSizeMake(EYScreenWidth, EYBackImageViewHeight);
+        [self.view addSubview:backImageView];
+        self.backImageView = backImageView;
+        
+        //2.UITableView
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, EYScreenWidth, EYScreenHeight - EYTabBarHomeIndicatorHeight) style:UITableViewStylePlain];
+        tableView.showsHorizontalScrollIndicator = NO;
+        tableView.showsVerticalScrollIndicator = YES;
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        //背景颜色
+        tableView.backgroundColor = EYColorClear;
+        //取消分割线
+        tableView.separatorStyle = UITableViewCellSelectionStyleNone;
+        tableView.rowHeight = UITableViewAutomaticDimension;
+        tableView.estimatedRowHeight = 100;
+        // 设置偏移量为0
+        if (@available(iOS 11.0, *)) {
+            tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = NO;
+        }
+        tableView.contentInset = UIEdgeInsetsMake(EYBackImageViewHeight * 0.5, 0, 0, 0);
+        [self.view addSubview:tableView];
+        self.tableView = tableView;
+        
+        //3.底部 view
         UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, EYScreenHeight - EYTabBarHomeIndicatorHeight, EYScreenWidth, EYTabBarHomeIndicatorHeight)];
         bottomView.backgroundColor = EYColorBlack;
         [self.view addSubview:bottomView];
+    } else {
+        //2.UITableView
+        UITableView *tabelView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, EYScreenWidth, EYScreenHeight - EYTabBarHomeIndicatorHeight) style:UITableViewStyleGrouped];
+        [self.view addSubview:tabelView];
+        self.tableView = tabelView;
     }
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.arrayM.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:EYMineViewControllerCellID];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:EYMineViewControllerCellID];
+    }
+    
+    cell.backgroundColor = EYColorRandom;
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    EYLog(@"12345678943==%ld", indexPath.row);
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // 向下拽了多少距离
+    CGFloat contentOffsetY = -scrollView.contentOffset.y;
+    if (contentOffsetY <= EYBackImageViewHeight * 0.5) {//起始位置
+        EYLog(@"起始位置==%f", contentOffsetY);
+//        self.backImageView.frame = CGRectMake(0, 0, EYScreenWidth, EYBackImageViewHeight);
+    } else if (contentOffsetY >= EYBackImageViewHeight) {//向下拽的最大位置
+        scrollView.contentOffset = CGPointMake(0, -EYBackImageViewHeight);
+        CGFloat scale = EYBackImageViewHeight * 0.4;
+        EYLog(@"向下拽的最大位置==%f==%f", contentOffsetY, scale);
+//        self.backImageView.frame = CGRectMake(-scale * 0.5, 0, EYScreenWidth + scale, EYBackImageViewHeight + scale);
+    } else {//需要放大图片
+        CGFloat scale = (1 - (EYBackImageViewHeight - contentOffsetY) / (EYBackImageViewHeight * 0.5)) * 0.4;
+//        self.backImageView.center = CGPointMake(EYScreenWidth * 0.5, 0);
+        self.backImageView.bounds = CGRectMake(0, 0, EYScreenWidth * (1 + scale), EYBackImageViewHeight * (1 + scale));
+    }
+}
+
+#pragma mark - 懒加载
+- (NSMutableArray *)arrayM {
+    if (nil == _arrayM) {
+        _arrayM = [NSMutableArray array];
+        for (int i = 0; i < 100; i++) {
+            [_arrayM addObject:@"1"];
+            [_arrayM addObject:@"2"];
+        }
+    }
+    return _arrayM;
 }
 
 @end
