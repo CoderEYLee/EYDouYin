64dee5fdc314a3a22ce677e50bbf4d8b387e9583
CoderEYLee <lieryangios@126.com>
2019-06-29 18:04:31 +0800

    修改

--- EYDouYin/EYDouYin/Class/Module/Me/Setting/微博(Swift)/Base(基类)/NavigationController/EYNavigationSwiftController.swift
+++ EYDouYin/EYDouYin/Class/Module/Me/Setting/微博(Swift)/Base(基类)/NavigationController/EYNavigationSwiftController.swift
@@ -12,9 +12,6 @@ class EYNavigationSwiftController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 隐藏默认的 NavigationBar
-        navigationBar.isHidden = true
     }
     
     /// 重写 push 方法，所有的 push 动作都会调用此方法！
@@ -25,28 +22,8 @@ class EYNavigationSwiftController: UINavigationController {
         if children.count > 0 {
             // 隐藏底部的 TabBar
             viewController.hidesBottomBarWhenPushed = true
-            
-            // 判断控制器的类型
-            if let vc = viewController as? EYBaseSwiftViewController {
-                
-                var title = "返回"
-                
-                // 判断控制器的级数，只有一个子控制器的时候，显示栈底控制器的标题
-                if children.count == 1 {
-                    // title 显示首页的标题
-                    title = children.first?.title ?? "返回"
-                }
-                
-                // 取出自定义的 navItem，设置左侧按钮作为返回按钮
-                vc.navItem.leftBarButtonItem = UIBarButtonItem(title: title, target: self, action: #selector(popToParent), isBack: true)
-            }
         }
         
         super.pushViewController(viewController, animated: true)
-    }
-    
-    /// POP 返回到上一级控制器
-    @objc private func popToParent() {
-        popViewController(animated: true)
     }
 }
