d90cbd45810b57b1d7ab2b0a9a131276caf13b59
CoderEYLee <lieryangios@126.com>
2019-07-02 18:36:24 +0800

    Update main.dart

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,6 +35,40 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
+
+      print('flutter的log打印：现在输出count=$_counter');
+      // 当个数累积到3的时候给客户端发参数
+      if(_counter == 3) {
+        _toNativeSomethingAndGetInfo();
+      }
+
+      // 当个数累积到5的时候给客户端发参数
+      if(_counter == 1002) {
+        Map<String, String> map = { "title": "这是一条来自flutter的参数" };
+        methodChannel.invokeMethod('toNativePush',map);
+      }
+
+      // 当个数累积到8的时候给客户端发参数
+      if(_counter == 1005) {
+        Map<String, dynamic> map = { "content": "flutterPop回来","data":[1,2,3,4,5]};
+        methodChannel.invokeMethod('toNativePop',map);
+      }
+    });
+  }
+
+  // 给客户端发送一些东东 , 并且拿到一些东东
+  Future<Null> _toNativeSomethingAndGetInfo() async {
+    dynamic result;
+    try {
+      result = await methodChannel.invokeMethod('toNativeSomething','大佬你点击了$_counter下');
+    } on PlatformException {
+      result = 100000;
+    }
+    setState(() {
+      // 类型判断
+      if (result is int) {
+        _counter = result;
+      }
     });
   }
 
