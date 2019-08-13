99589d86e6a3010457bd4cb802a2a2d9a2e0b169
CoderEYLee <lieryangios@126.com>
2019-08-13 16:38:03 +0800

    显示数据

--- ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/UICollectionView/EYCustomViewController1.m
+++ ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/UICollectionView/EYCustomViewController1.m
@@ -8,11 +8,17 @@
 
 #import "EYCustomViewController1.h"
 
-@interface EYCustomViewController1 ()
+@interface EYCustomViewController1 () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) UICollectionView *collectionView;
+@property (strong, nonatomic) NSMutableArray *arrayM;
+
 
 @end
 
 @implementation EYCustomViewController1
+
+static NSString *EYCustomViewController1CellID = @"EYCustomViewController1CellID";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,7 +29,75 @@
 
 //1.初始化界面
 - (void)setupUI {
-    EYLog(@"12errtupio");
+    [self.collectionView reloadData];
+}
+
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.arrayM.count;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:EYCustomViewController1CellID forIndexPath:indexPath];;
+//    cell.videoModel = self.arrayM[indexPath.row];
+    cell.backgroundColor = EYColorRandom;
+    
+    return cell;
+}
+
+//设置每个item的尺寸
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGFloat width = (EYScreenWidth - 6) / 3;
+    CGFloat height = width * 4 / 3;
+    return CGSizeMake(width, height);
+}
+
+// 设置每个item的UIEdgeInsets
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(1, 1, 0, 1);
+}
+
+//设置每个item水平间距
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+//设置每个item垂直间距
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 2;
+}
+
+#pragma mark - UICollectionViewDelegate
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    EYLog(@"12it5yue56u");
+}
+
+#pragma mark - 懒加载
+- (UICollectionView *)collectionView {
+    if (nil == _collectionView) {
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+        layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, EYStatusBarAndNaviBarHeight, EYScreenWidth, EYScreenHeight - EYStatusBarAndNaviBarHeight) collectionViewLayout:layout];
+        collectionView.showsHorizontalScrollIndicator = YES;
+        collectionView.showsVerticalScrollIndicator = YES;
+        collectionView.dataSource = self;
+        collectionView.delegate = self;
+        collectionView.backgroundColor = EYColorClear;
+        [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:EYCustomViewController1CellID];
+        [self.view addSubview:collectionView];
+        _collectionView = collectionView;
+    }
+    return _collectionView;
+}
+
+- (NSMutableArray *)arrayM {
+    if (nil == _arrayM) {
+        _arrayM = [NSMutableArray array];
+        for (int i = 0; i < 100; i++) {
+            [_arrayM addObject:[NSString stringWithFormat:@"%d", i]];
+        }
+    }
+    return _arrayM;
 }
 
 @end
