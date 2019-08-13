7b3d69f705ad97f1c2404c7b6260705a7aa64519
CoderEYLee <lieryangios@126.com>
2019-08-13 15:49:36 +0800

    修改

---
+++ ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/UICollectionView/EYCustomViewController1.m
@@ -0,0 +1,32 @@
+//
+//  EYCustomViewController1.m
+//  EYDouYin
+//
+//  Created by 李二洋 on 2019/8/13.
+//  Copyright © 2019 李二洋. All rights reserved.
+//
+
+#import "EYCustomViewController1.h"
+
+@interface EYCustomViewController1 ()
+
+@end
+
+@implementation EYCustomViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
