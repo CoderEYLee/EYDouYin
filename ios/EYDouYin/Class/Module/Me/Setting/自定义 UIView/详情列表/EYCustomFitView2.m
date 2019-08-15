55869ea36a94ebb42cb2c7d2d4d1c732fd5e69da
CoderEYLee <lieryangios@126.com>
2019-08-15 15:10:00 +0800

    修改

--- ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/EYCustomFitView2.m
+++ ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/EYCustomFitView2.m
@@ -8,6 +8,36 @@
 
 #import "EYCustomFitView2.h"
 
+@interface EYCustomFitView2()
+
+@property (weak, nonatomic) UILabel *tt_titleLabel;
+
+@end
+
 @implementation EYCustomFitView2
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        UILabel *tt_titleLabel = [[UILabel alloc] init];
+        tt_titleLabel.numberOfLines = 0;
+        tt_titleLabel.backgroundColor = EYColorBlue;
+        [self addSubview:tt_titleLabel];
+        self.tt_titleLabel = tt_titleLabel;
+        [tt_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.left.mas_equalTo(10);
+            make.bottom.right.mas_equalTo(-10);
+        }];
+    }
+    return self;
+}
+
+- (void)setVideoModel:(EYVideoModel *)videoModel {
+    _videoModel = videoModel;
+    
+    self.tt_titleLabel.text = videoModel.video_title;
+    
+    self.frame = CGRectMake(0, 0, EYScreenWidth, 200 + 20);
+}
 
 @end
