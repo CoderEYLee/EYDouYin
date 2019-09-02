edae00363f0f6a13d096c772718231f8a91913eb
CoderEYLee <lieryangios@126.com>
2019-09-02 10:01:49 +0800

    增加

---
+++ ios/EYDouYin/Class/Module/Me/Setting/UITableView分组/EYTableViewSizeToFitViewController.m
@@ -0,0 +1,88 @@
+//
+//  EYTableViewSizeToFitViewController.m
+//  EYDouYin
+//
+//  Created by 李二洋 on 2019/8/30.
+//  Copyright © 2019 李二洋. All rights reserved.
+//
+
+#import "EYTableViewSizeToFitViewController.h"
+
+@interface EYTableViewSizeToFitViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *arrayM;
+
+@end
+
+@implementation EYTableViewSizeToFitViewController
+
+static NSString *EYTableViewSizeToFitViewControllerCellID = @"EYTableViewSizeToFitViewControllerCellID";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //1.初始化界面
+    [self setupUI];
+}
+
+- (void)setupUI {
+    self.view.backgroundColor = EYColorRandom;
+    
+    [self.tableView reloadData];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.arrayM.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:EYTableViewSizeToFitViewControllerCellID];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:EYTableViewSizeToFitViewControllerCellID];
+    }
+    
+    cell.backgroundColor = EYColorRandom;
+    
+    return cell;
+}
+
+#pragma mark - 懒加载
+- (UITableView *)tableView {
+    if (nil == _tableView) {
+        UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, EYStatusBarAndNaviBarHeight, EYScreenWidth, EYScreenHeight - EYStatusBarAndNaviBarHeight) style:UITableViewStylePlain];
+        tableView.backgroundColor = EYColorClear;
+        tableView.rowHeight = UITableViewAutomaticDimension;
+        tableView.estimatedRowHeight = 44;
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        [self.view addSubview:tableView];
+        
+        if (@available(iOS 11.0, *)) {
+            tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = NO;
+        }
+        
+        _tableView = tableView;
+    }
+    return _tableView;
+}
+
+- (NSMutableArray *)arrayM {
+    if (nil == _arrayM) {
+        _arrayM = [NSMutableArray array];
+        for (int i = 0; i < 100; i++) {
+            [_arrayM addObject:[NSString stringWithFormat:@"%d", i]];
+        }
+    }
+    return _arrayM;
+}
+
+@end
