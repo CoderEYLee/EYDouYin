24e790ab9c31626e151c812427c0c82741e44b2a
CoderEYLee <lieryangios@126.com>
2019-09-17 16:51:56 +0800

    修改

--- ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/6、UIPageViewController/EYPageViewController.m
+++ ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/6、UIPageViewController/EYPageViewController.m
@@ -12,6 +12,7 @@
 @interface EYPageViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate>
 
 @property (weak, nonatomic) UIPageViewController *pageViewController;
+@property (strong, nonatomic) NSMutableArray *arrayM;
 
 @end
 
@@ -26,24 +27,83 @@
 
 //1.初始化界面
 - (void)setupUI {
-    UIPageViewController *pageViewController = [[UIPageViewController alloc] init];
+    UIPageViewController *pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:@{UIPageViewControllerOptionInterPageSpacingKey: @(-100)}];
     pageViewController.dataSource = self;
     pageViewController.delegate = self;
     pageViewController.view.backgroundColor = EYColorRed;
     [self.view addSubview:pageViewController.view];
     [self addChildViewController:pageViewController];
     self.pageViewController = pageViewController;
+    
+    [pageViewController setViewControllers:@[self.arrayM.firstObject] direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
 }
 
 #pragma mark - UIPageViewControllerDataSource
 
 - (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
-    return nil;
+    EYLog(@"33333333333");
+    NSUInteger index = [self.arrayM indexOfObject:viewController];
+    if (index == 0 || (index == NSNotFound)) {
+        return nil;
+    }
+    return self.arrayM[--index];
 }
 - (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
-    return nil;
+    EYLog(@"4444444444444");
+    NSUInteger index = [self.arrayM indexOfObject:viewController];
+    if (index == self.arrayM.count - 1 || (index == NSNotFound)) {
+        return nil;
+    }
+
+    return self.arrayM[++index];
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController {
+    return self.arrayM.count;
+}
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController {
+    return 0;
 }
 
 #pragma mark - UIPageViewControllerDelegate
+- (void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray<UIViewController *> *)pendingViewControllers {
+    UIViewController *nextVC = [pendingViewControllers firstObject];
+    NSInteger index = [self.arrayM indexOfObject:nextVC];
+    
+     EYLog(@"开始==%@==%ld", pendingViewControllers, index);
+}
+
+// Sent when a gesture-initiated transition ends. The 'finished' parameter indicates whether the animation finished, while the 'completed' parameter indicates whether the transition completed or bailed out (if the user let go early).
+- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray<UIViewController *> *)previousViewControllers transitionCompleted:(BOOL)completed {
+    UIViewController *nextVC = [previousViewControllers firstObject];
+    NSInteger index = [self.arrayM indexOfObject:nextVC];
+    EYLog(@"结束==%@==上一个下标:%ld==是否结束:%d==是否完成:%d", previousViewControllers, index, finished, completed);
+}
+
+//- (UIPageViewControllerSpineLocation)pageViewController:(UIPageViewController *)pageViewController spineLocationForInterfaceOrientation:(UIInterfaceOrientation)orientation {
+//
+//}
+
+//- (UIInterfaceOrientationMask)pageViewControllerSupportedInterfaceOrientations:(UIPageViewController *)pageViewController {
+//
+//}
+//- (UIInterfaceOrientation)pageViewControllerPreferredInterfaceOrientationForPresentation:(UIPageViewController *)pageViewController {
+//
+//}
+
+#pragma mark - 懒加载
+
+- (NSMutableArray *)arrayM {
+    if (nil == _arrayM) {
+        _arrayM = [NSMutableArray array];
+        for (int i = 0; i < 3; i++) {
+            EYTestViewController *vc = [[EYTestViewController alloc] init];
+            vc.view.backgroundColor = EYColorRandom;
+            vc.label.text = [NSString stringWithFormat:@"%d", i];
+            [_arrayM addObject:vc];
+        }
+    }
+    return _arrayM;
+}
 
 @end
