0b001918f00d6a350b567d76863c37401b7be32a
CoderEYLee <lieryangios@126.com>
2019-09-26 14:05:27 +0800

    增加三方

--- ios/EYDouYin/Class/Module/Message/EYMessageViewController.m
+++ ios/EYDouYin/Class/Module/Message/EYMessageViewController.m
@@ -29,7 +29,18 @@
     self.gk_navLineHidden = YES;
     self.view.backgroundColor = EYColorRandom;
     
-    //1.底部 view
+    XRCarouselView *carouselView = [[XRCarouselView alloc] initWithFrame:CGRectMake(0, 100, EYScreenWidth, 300)];
+    carouselView.placeholderImage = [UIImage imageNamed:@"common_placeholder"];
+    carouselView.time = 1;
+    carouselView.imageArray = @[@"https:/videoali.chinlab.com/screenshot/45441559010378.jpg", @"https:/videoali.chinlab.com/screenshot/45441558422766.jpg"];
+    carouselView.backgroundColor = EYColorRandom;
+    [self.view addSubview:carouselView];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        carouselView.imageArray = @[@"https:/videoali.chinlab.com/screenshot/45441559010378.jpg", @"https:/videoali.chinlab.com/screenshot/45441558422766.jpg", @"https:/videoali.chinlab.com/screenshot/45441558421673.jpg", @"https:/videoali.chinlab.com/screenshot/45441558421328.jpg"];
+    });
+    
+    //2.底部 view
     UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, EYScreenHeight - EYTabBarHomeIndicatorHeight, EYScreenWidth, EYTabBarHomeIndicatorHeight)];
     bottomView.backgroundColor = EYColorBlack;
     [self.view addSubview:bottomView];
