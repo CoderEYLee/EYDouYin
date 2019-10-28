6cbf18c43acc8651f2175f0933d12bd670aa39f5
CoderEYLee <lieryangios@126.com>
2019-10-28 11:52:10 +0800

    增加轮播

---
+++ ios/EYDouYin/Class/Module/Me/Setting/自定义 UIView/详情列表/7、轮播/EYXRCarouselViewController.h
@@ -0,0 +1,17 @@
+//
+//  EYXRCarouselViewController.h
+//  EYDouYin
+//
+//  Created by 李二洋 on 2019/10/28.
+//  Copyright © 2019 李二洋. All rights reserved.
+//
+
+#import "EYBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EYXRCarouselViewController : EYBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
