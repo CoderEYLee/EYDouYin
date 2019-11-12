672cf9a0076c30b5d556717629f826db45df3322
CoderEYLee <lieryangios@126.com>
2019-11-12 10:58:01 +0800

    增加三方

--- ios/EYDouYin/Class/Module/Message/EYMessageViewController.m
+++ ios/EYDouYin/Class/Module/Message/EYMessageViewController.m
@@ -27,7 +27,15 @@
 //1. 初始化界面
 - (void)setupUI {
     self.gk_navLineHidden = YES;
-    self.view.backgroundColor = EYColorRandom;
+    self.gk_navigationBar.backgroundColor = EYColorBlue;
+    
+    //1.滚动视图
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.gk_navigationBar.height, EYScreenWidth, EYScreenHeight)];
+    scrollView.backgroundColor = EYColorRed;
+    scrollView.contentSize = CGSizeMake(EYScreenWidth, EYScreenHeight * 2);
+    scrollView.showsVerticalScrollIndicator = YES;
+    scrollView.mj_header = [MJRefreshHeader headerWithRefreshingTarget:self refreshingAction:@selector(refresh)];
+    [self.view addSubview:scrollView];
     
     //2.底部 view
     UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, EYScreenHeight - EYTabBarHomeIndicatorHeight, EYScreenWidth, EYTabBarHomeIndicatorHeight)];
