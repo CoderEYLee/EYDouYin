ea7331e6912c967a26cc288728f4bc469b526086
lieryang <lieryang@bytedance.com>
2021-07-04 16:33:06 +0800

    删除本地加密功能

--- ios/EYDouYin/Class/Module/Me/Setting/Collection(自己收藏)/EYCollectionViewController.m
+++ ios/EYDouYin/Class/Module/Me/Setting/Collection(自己收藏)/EYCollectionViewController.m
@@ -58,33 +58,7 @@ static NSString *EYCollectionViewControllerCellID = @"EYCollectionViewController
     EYLocalUseModel *localUseModel = self.arrayM[indexPath.row];
     EYCollectionDetailViewController *vc = [[EYCollectionDetailViewController alloc] init];
     vc.localUseModel = localUseModel;
-    if (localUseModel.lock) {
-        //添加提示框
-        UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"您正在访问内部优惠券" preferredStyle:UIAlertControllerStyleAlert];
-        __weak typeof(alert) weakAlert = alert;
-        [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
-            textField.secureTextEntry = YES;
-            textField.keyboardType = UIKeyboardTypeNumberPad;
-        }];
-        [alert addTextFieldWithConfigurationHandler:nil];
-        [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
-        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
-            __strong typeof(weakAlert) alert = weakAlert;
-            NSString *password = alert.textFields.firstObject.text;
-            if ([password.md2String isEqualToString:@"ec959feaadc86988b166247cd670dbaf"]) {
-                NSString *text = alert.textFields.lastObject.text;
-                if (text.length) {
-                    NSString *content_url = localUseModel.content_url;
-                    localUseModel.content_url = [content_url stringByReplacingOccurrencesOfString:@"baidu" withString:text options:NSRegularExpressionSearch range:NSMakeRange(0, content_url.length - 1)];
-                    vc.localUseModel = localUseModel;
-                }
-                [self.navigationController pushViewController:vc animated:YES];
-            }
-        }]];
-        [self presentViewController:alert animated:YES completion:nil];
-    } else {
-        [self.navigationController pushViewController:vc animated:YES];
-    }
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 - (UITableView *)tableView {
